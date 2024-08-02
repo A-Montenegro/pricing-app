abce685eee26c02464e95e7cfec6885b44ea2884
Alberto Martínez Montenegro <albertomartinez44@hotmail.com>
2024-08-02 19:52:30 +0200

    Initial structure creation

---
+++ main/api-rest/src/test/java/com/gft/technicaltest/application/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.technicaltest.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
